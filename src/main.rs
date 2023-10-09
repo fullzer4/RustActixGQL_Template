dd637f98c6ee296e47e5d747963b07faa995415f
fullzer4 <gabrielpelizzaro@gmail.com>
2023-10-09 03:39:05 -0300

    Routes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{get, App, HttpServer};
+
+mod routes;
+use crate::routes::ping::Rping;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
+HttpServer::new(|| App::new()
+        .service(Rping)
+    )
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
